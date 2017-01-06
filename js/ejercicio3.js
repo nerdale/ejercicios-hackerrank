ede86168dc91f46e46bd1cb5b6c1c6c7a054b7e0
“nerdale” <alexandra.neira@gmail.com>
2017-01-06 10:09:06 -0300

    exercise time convert adding toUpperCase and parseInt

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,22 +1,22 @@
 function timeConvert(parametro){
     var parametro = prompt("Ingrese una hora (hora + minutos + segundos) ejemplo: 07:05:45PM");
     var horacompleta = parametro.split(":");
-    var hora = Number(horacompleta[0]);
+    var hora = parseInt(horacompleta[0]);
     var minutos = horacompleta[1];
     var segundos = horacompleta[2].substr(0,2);
     var ampm = horacompleta[2].substr(2);
 
-    if ( ampm === "AM"){
-        if (hora === 12){
+    if(ampm.toUpperCase() === "AM"){
+        if(hora === 12){
             hora = 0;
         }
     }else{
-        if ( hora !== 12){
+        if(hora !== 12){
             hora += 12;
         }
     }
-    if ( hora < 10){
-        hora = "0 " + hora;
+    if(hora < 10){
+        hora = "0" + hora;
     }
     document.write(hora + ":" + minutos + ":" + segundos);
 }
@@ -46,4 +46,5 @@ function main() {
     }
     console.log(hora + ":" + minutos + ":" + segundos);
 }
+
 */

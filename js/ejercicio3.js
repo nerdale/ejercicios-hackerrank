579fcb60a921f60457b14a132ad0456ae11aa4fb
“nerdale” <alexandra.neira@gmail.com>
2017-01-06 10:12:04 -0300

    exercise time convert adding toUpperCase and parseInt

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,16 +1,11 @@
 function timeConvert(parametro){
     var parametro = prompt("Ingrese una hora (hora + minutos + segundos) ejemplo: 07:05:45PM");
     var horacompleta = parametro.split(":");
-<<<<<<< HEAD
     var hora = parseInt(horacompleta[0]);
-=======
-    var hora = Number(horacompleta[0]);
->>>>>>> 1c73c757d431c195b283d0cdaaf7d015e3116dde
     var minutos = horacompleta[1];
     var segundos = horacompleta[2].substr(0,2);
     var ampm = horacompleta[2].substr(2);
 
-<<<<<<< HEAD
     if(ampm.toUpperCase() === "AM"){
         if(hora === 12){
             hora = 0;
@@ -22,32 +17,12 @@ function timeConvert(parametro){
     }
     if(hora < 10){
         hora = "0" + hora;
-=======
-    if ( ampm === "AM"){
-        if (hora === 12){
-            hora = 0;
-        }
-    }else{
-        if ( hora !== 12){
-            hora += 12;
-        }
-    }
-    if ( hora < 10){
-        hora = "0 " + hora;
->>>>>>> 1c73c757d431c195b283d0cdaaf7d015e3116dde
     }
     document.write(hora + ":" + minutos + ":" + segundos);
 }
 
-<<<<<<< HEAD
-
 /*
 Funcion Hackerrank
-=======
-/*
-Funcion Hackerrank 
->>>>>>> 1c73c757d431c195b283d0cdaaf7d015e3116dde
-
 function main() {
     var time = readLine();
     var horacompleta = time.split(":");
@@ -55,7 +30,6 @@ function main() {
     var minutos = horacompleta[1];
     var segundos = horacompleta[2].substr(0,2);
     var ampm = horacompleta[2].substr(2);
-
     if ( ampm === "AM"){
         if (hora === 12){
             hora = 0;
@@ -70,8 +44,4 @@ function main() {
     }
     console.log(hora + ":" + minutos + ":" + segundos);
 }
-<<<<<<< HEAD
-
-=======
->>>>>>> 1c73c757d431c195b283d0cdaaf7d015e3116dde
 */
